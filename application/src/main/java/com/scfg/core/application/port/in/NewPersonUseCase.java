decbfc41d926264e5e5bb787d394d4dd650e2823
Natalia Duran <nduran@santacruzfg.com>
2023-08-08 14:29:11 -0400

    Cambios por la migración de personas

--- application/src/main/java/com/scfg/core/application/port/in/NewPersonUseCase.java
+++ application/src/main/java/com/scfg/core/application/port/in/NewPersonUseCase.java
@@ -2,9 +2,6 @@ package com.scfg.core.application.port.in;
 import com.scfg.core.domain.person.NewPerson;
 
 public interface NewPersonUseCase {
-//     List<NewPerson> getAll();
-//     NewPerson getById(long personId);
-//     Boolean update(NewPerson person);
     Object searchPerson(Long docType, String documentNumber, String name);
     Boolean saveOrUpdate(NewPerson newPerson);
 
